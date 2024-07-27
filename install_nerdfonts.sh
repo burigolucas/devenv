2b6bd6c902a2c0aa21a10fe7c7215b6b86c9e30a
Lucas Burigo <burigolucas@gmail.com>
2024-07-27 13:51:16 +0200

    Add starship and nerdfonts

---
+++ install_nerdfonts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# See https://www.nerdfonts.com/#home
+mkdir -p ~/.local/share/fonts/NerdFonts
+cd ~/.local/share/fonts/NerdFonts
+
+curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
